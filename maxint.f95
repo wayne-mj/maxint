a2e6a813f3d2002279b859b0186bcd77124b8839
Wayne.MJ <150761502+wayne-mj@users.noreply.github.com>
2025-12-29 19:25:18 +1000

    Small change

--- maxint.f95
+++ maxint.f95
@@ -1,11 +1,12 @@
 program maxintprog
     use iso_fortran_env, only: int16, int32, int64
+    
     implicit none
 
-    integer(int16), parameter :: int16_integer = 1_int16
-    integer(int32), parameter :: int32_integer = 1_int32
-    integer(int64), parameter :: int64_integer = 1_int64
-
+    integer(kind=int16), parameter :: int16_integer = 1_int16
+    integer(kind=int32), parameter :: int32_integer = 1_int32
+    integer(kind=int64), parameter :: int64_integer = 1_int64
+    
     print *, "Max positive int16 Integer", huge(int16_integer)
     print *, "Max negative int16 Integer", -huge(int16_integer)
 
